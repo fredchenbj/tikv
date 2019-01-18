5cac05a27b97b8c03cb32ccf2300cfecd2669662
fredchenbj <cfworking@163.com>
2019-01-18 17:54:02 +0800

    add update_merge operator and update api
    Signed-off-by: fredchenbj <cfworking@163.com>

--- src/raftstore/store/peer.rs
+++ src/raftstore/store/peer.rs
@@ -2012,9 +2012,11 @@ pub trait RequestInspector {
         for r in req.get_requests() {
             match r.get_cmd_type() {
                 CmdType::Get | CmdType::Snap => has_read = true,
-                CmdType::Delete | CmdType::Put | CmdType::DeleteRange | CmdType::IngestSST => {
-                    has_write = true
-                }
+                CmdType::Delete
+                | CmdType::Put
+                | CmdType::Update
+                | CmdType::DeleteRange
+                | CmdType::IngestSST => has_write = true,
                 CmdType::Prewrite | CmdType::Invalid => {
                     return Err(box_err!(
                         "invalid cmd type {:?}, message maybe corrupted",
@@ -2196,6 +2198,7 @@ impl ReadExecutor {
                 }
                 CmdType::Prewrite
                 | CmdType::Put
+                | CmdType::Update
                 | CmdType::Delete
                 | CmdType::DeleteRange
                 | CmdType::IngestSST
