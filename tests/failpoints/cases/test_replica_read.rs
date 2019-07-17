7b8749c8ba1a367015c1ce6fe4720d0ecb3ef16d
5kbpers <20279863+5kbpers@users.noreply.github.com>
2019-07-17 13:35:48 +0800

    support follower read (#5051)
    * raftstore,server: support follower read

--- tests/failpoints/cases/test_replica_read.rs
+++ tests/failpoints/cases/test_replica_read.rs
@@ -3,10 +3,6 @@
 use std::sync::Arc;
 
 use fail;
-use std::mem;
-use std::sync::atomic::AtomicBool;
-use std::sync::Mutex;
-use std::thread;
 use std::time::Duration;
 use test_raftstore::*;
 use tikv_util::HandyRwLock;
@@ -67,6 +63,7 @@ fn test_wait_for_apply_index() {
         Err(_) => panic!("follower read failed"),
     }
 }
+<<<<<<< HEAD
 
 #[test]
 fn test_duplicate_read_index_ctx() {
@@ -139,3 +136,5 @@ fn test_duplicate_read_index_ctx() {
     assert!(rx2.recv_timeout(Duration::from_millis(500)).is_ok());
     assert!(rx3.recv_timeout(Duration::from_millis(500)).is_ok());
 }
+=======
+>>>>>>> c759022b... Rename follower read (#5118)
